4288c6581a9531736a7e9a5dfc66f0f730cf8039
kdiab <khalid@diab.cc>
2024-08-23 21:44:58 -0400

    improved color by using original lolcat function

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 # Usage
 
 This program is inteded to be used with programs that output text, e.g: fortune, cowsay, cat and so on.
+
 Usage: echo "Hello, world!" | lolcat
--- lolcat.go
+++ lolcat.go
@@ -4,19 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
 )
 
+func rgb(c int) (int, int, int) {
+	// original implementation from https://github.com/busyloop/lolcat/blob/master/lib/lolcat/lol.rb#L36
+	freq := 0.1
+	r := int(math.Sin(freq*float64(c)+0)*127 + 128)
+	g := int(math.Sin(freq*float64(c)+2*math.Pi/3)*127 + 128)
+	b := int(math.Sin(freq*float64(c)+4*math.Pi/3)*127 + 128)
+	return r, g, b
+}
+
 func print(output []rune) {
-	colors := []string{}
-
-	for i := 31; i < 37; i++ {
-		s := "\033[" + fmt.Sprintf("%d", i) + "m"
-		colors = append(colors, s)
-	}
-
 	for j := 0; j < len(output); j++ {
-		fmt.Printf("%s%c", colors[j%len(colors)], output[j])
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
 	}
 
 	fmt.Println()
